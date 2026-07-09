4dd32b8ed82b7f549f1e24c2cffd4512b893e291
Alexey Novarov <alexey.novarov@gmail.com>
2026-07-09 17:25:17 +0300

    get_currencies - added HTTP exception

--- firstTry.py
+++ firstTry.py
@@ -2,7 +2,11 @@ import requests
 
 def get_currencies() -> list:
     base_url = 'https://api.nbrb.by/exrates/currencies'
-    response = requests.get(base_url, timeout=5)
-    return response.json()
+    try:
+        response = requests.get(base_url, timeout=5)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.HTTPError as e:
+        raise f'HTTP error: {e}'
 
 print(get_currencies())
