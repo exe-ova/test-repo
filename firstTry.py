13026e18c14914994a007a4fdfe4de98233594b3
Alexey Novarov <alexey.novarov@gmail.com>
2026-07-09 17:14:15 +0300

    el removed

--- firstTry.py
+++ firstTry.py
@@ -2,9 +2,7 @@ import requests
 
 def get_currencies() -> list:
     base_url = 'https://api.nbrb.by/exrates/currencies'
-
     response = requests.get(base_url, timeout=5)
-
     return response.json()
 
 print(get_currencies())
